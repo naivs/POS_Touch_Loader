1d9ce856ead1e871144879dc628c400796850278
ivan <ivan@primary>
2017-10-29 19:30:09 +0300

    Removed creationDate, modifiedDate, accessibleSubgroups and isValid fields

--- Touch_Emulator/src/main/java/data/Product.java
+++ Touch_Emulator/src/main/java/data/Product.java
@@ -1,8 +1,6 @@
 package data;
 
 import java.io.Serializable;
-import java.util.Calendar;
-import java.util.List;
 
 /**
  *
@@ -13,22 +11,11 @@ public class Product implements Serializable {
     private final String name;
     private final String plu;
     private final String picturePath;
-    private List<String> accessibleSubgroups;
-    private boolean isValid;
-    private final String creationDate;
 
-    public Product(String name, String plu, String picturePath, List<String> accessibleSubgroups) {
-        
-        this(name, plu, picturePath, accessibleSubgroups, Calendar.getInstance().getTime().toString());
-    }
-
-    public Product(String name, String plu, String picturePath, List<String> accessibleSubgroups, String creationDate) {
-        
+    public Product(String name, String plu, String picturePath) {
         this.name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
         this.plu = plu;
         this.picturePath = picturePath;
-        this.accessibleSubgroups = accessibleSubgroups;
-        this.creationDate = creationDate;
     }
 
     public String getPlu() {
@@ -39,28 +26,8 @@ public class Product implements Serializable {
         return picturePath;
     }
 
-    public List<String> getAccessibleSubgroups() {
-        return accessibleSubgroups;
-    }
-
-    public String getCreationDate() {
-        return creationDate;
-    }
-
-    public void setAccessibleSubgroups(List<String> accessibleSubgroups) {
-        this.accessibleSubgroups = accessibleSubgroups;
-    }
-
-    public void setRemoved(boolean statement) {
-        isValid = statement;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public boolean isValid() {
-        return isValid;
     }
 
     @Override

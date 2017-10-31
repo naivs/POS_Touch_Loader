a286ac055087985146d34f3abb0eb67f03bc638c
Ivan_HP <ivan@ivan-HP-Notebook>
2017-10-31 16:22:35 +0300

    This class is not used in project so far.

--- Touch_Emulator/src/main/java/utils/LoadAnalyzer.java
+++ Touch_Emulator/src/main/java/utils/LoadAnalyzer.java
@@ -1,80 +1,83 @@
-package utils;
+/*
+ * Copyright (C) 2017 Ivan Naumov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */package utils;
 
 import java.util.ArrayList;
 import data.TerminalGroup;
+import java.util.Arrays;
 
 /**
  *
  * @author ivan
  */
 public class LoadAnalyzer {
-
-    private float[][] load;
-
-    final int MAX_PROD = 20 * 8 * 8;
+    private final float[][] load; // load table (x - departments; y - days)
+    private final int MAX_MAG_PRODUCTS = 19_980;
+    //private final int MAX_POS_PRODUCTS = 320;
 
     public LoadAnalyzer(ArrayList<TerminalGroup> configuration) {
-
-        load = new float[7][MAX_PROD];
-        int[][] prodCount = new int[7][MAX_PROD];
+        load = new float[7][configuration.size()];
 
         for (int i = 0; i < configuration.size(); i++) {
             for (int j = 0; j < configuration.get(i).getDaysOfWeek().length; j++) {
-
-                prodCount[j][i] = 0;
-
-                for (int k = 0; k < configuration.get(i).getDaysOfWeek()[j].getGroupCount(); k++) {
-                    if (configuration.get(i).getDaysOfWeek()[j].getGroup(k) != null) {
-                        for (int l = 0; l < configuration.get(i).getDaysOfWeek()[j].getGroup(k).getSubgroupCount(); l++) {
-                            if (configuration.get(i).getDaysOfWeek()[j].getGroup(k).getSubgroup(l) != null) {
-                                prodCount[j][i] += configuration.get(i).getDaysOfWeek()[j].getGroup(k).getSubgroup(l).getProductCount();
+                int[] prodCount = new int[7];
+                Arrays.fill(prodCount, 0);
+                
+                data.DayOfWeek day = configuration.get(i).getDaysOfWeek()[j];
+                for (int k = 0; k < day.getGroupCount(); k++) {
+                    if (day.getGroup(k) != null) {
+                        for (int l = 0; l < day.getGroup(k).getSubgroupCount(); l++) {
+                            if (day.getGroup(k).getSubgroup(l) != null) {
+                                prodCount[j] += 20;
                             }
                         }
                     }
                 }
-
-                load[j][i] = prodCount[j][i] / (MAX_PROD / 100f);
+                load[j][i] = (MAX_MAG_PRODUCTS / prodCount[j]) * 100f;
             }
         }
     }
 
     public float[][] getDaysLoad() {
-
         return load;
     }
 
     public boolean groupIsLoad(int terminalGroup) {
-
-        boolean isLoad = false;
-        
+        boolean isLoad = false;        
         for (int j = 0; j < load.length; j++) {
-
             if (load[j][terminalGroup] != 0) {
                 isLoad = true;
                 break;
             }
         }
-        
         return isLoad;
     }
     
     public boolean dayIsLoad(int day) {
-        
         boolean isLoad = false;
-        
         for (int j = 0; j < load[day].length; j++) {
-
             if (load[day][j] != 0) {
                 isLoad = true;
                 break;
             }
         }
-        
         return isLoad;
     }
     
     public boolean isLoad(int terminalGroup, int day) {
-        
         return load[day][terminalGroup] != 0;
     }
 }

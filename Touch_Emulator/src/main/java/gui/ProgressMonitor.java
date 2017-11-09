2111dff162d844561bcb65ac0d4787e4db13f21e
Ivan_HP <ivan@ivan-HP-Notebook>
2017-11-09 14:29:05 +0300

    Realized stepUp() method

--- Touch_Emulator/src/main/java/gui/ProgressMonitor.java
+++ Touch_Emulator/src/main/java/gui/ProgressMonitor.java
@@ -39,11 +39,16 @@ public class ProgressMonitor extends javax.swing.JDialog {
     
     public void addDescription(String description) {
         this.description = description;
+        if(description != null) {
+            jLabel1.setText(description);
+        }
     }
     
     public void stepUp() {
-        if(description != null) {
-            
+        int currentProgress = jProgressBar1.getValue();
+        if(currentProgress < 100) {
+            currentProgress++;
+            jProgressBar1.setValue(currentProgress);
         }
     }
 
@@ -60,10 +65,12 @@ public class ProgressMonitor extends javax.swing.JDialog {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setUndecorated(true);
+        setResizable(false);
 
         jProgressBar1.setStringPainted(true);
 
-        jLabel1.setText("jLabel1");
+        jLabel1.setPreferredSize(new java.awt.Dimension(8, 15));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -74,7 +81,7 @@ public class ProgressMonitor extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 588, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -82,7 +89,7 @@ public class ProgressMonitor extends javax.swing.JDialog {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(7, 7, 7)
-                .addComponent(jLabel1)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
